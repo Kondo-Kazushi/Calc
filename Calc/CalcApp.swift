c07d8c6e3efd2cc48568afe31165d45476ef2491
Kazu <romans121718kk@icloud.com>
2022-08-09 12:52:57 +0900

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by 近藤和志 on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
